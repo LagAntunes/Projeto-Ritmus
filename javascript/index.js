273cea664f4a7a90bbd0944517cc7acd42132d75
LagAntunes <lag151617@gmail.com>
2022-03-28 18:43:15 -0300

    adicionando jquery e animateScroll

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+let botaoPlanos = document.getElementById("botaoPlanos");
+
+botaoPlanos.onclick = function clicaBotao() {
+    $('.valorPlano1').animatescroll({scrollSpeed:200,easing:'easeInQuint'});
+}
