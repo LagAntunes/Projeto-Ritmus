aa7426e61f2f4ff1cfe67408cea284b3a0311fcb
LagAntunes <lag151617@gmail.com>
2022-03-29 21:45:01 -0300

    criação da section de formulário

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 let botaoPlanos = document.getElementById("botaoPlanos");
 
 botaoPlanos.onclick = function clicaBotao() {
-    $('.valorPlano1').animatescroll({scrollSpeed:200,easing:'easeInQuint'});
+    $('.planos').animatescroll({scrollSpeed:200,easing:'easeInQuint'});
 }
